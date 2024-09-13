126ff2bf7000a600246ef83e779e43aba49a9740
ssebastianvv <ssebastianvapi@gmail.com>
2024-09-13 07:54:15 -0500

    create a login

--- taller-like/src/app/controller/register.controller.tsx
+++ taller-like/src/app/controller/register.controller.tsx
@@ -3,7 +3,7 @@ import { IUser, IUserToken } from '../interfaces/auth.interface';
 
 export const registerUser = async (name: string, email: string, password: string): Promise<{ user: IUser | null, message: string }> => {
   try {
-    const response = await fetch('https://simuate-test-backend-1.onrender.com/api/users/', {
+    const response = await fetch('https://simuate-test-backend-1.onrender.com/api/auth/register', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ name, email, password }),
